b9c24e6cc9dafedb81fe9a93496bf7b3fd1cd1f2
CEPIII <seroboy@yandex.ru>
2020-12-07 02:59:39 +0300

    Заполнить массив рандомными значениями по заданному ранее диапазону

--- FirstTemp/Program.cs
+++ FirstTemp/Program.cs
@@ -6,7 +6,15 @@ namespace FirstTemp
     {
         static void Main(string[] args)
         {
-            
+            Console.Write("Введите положительный диапазон значений случайных чисел, которые заполнят массив от 0 \n diapazon = ");
+            int diapazon = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Определите первый размер двумерного массива : \n\t[x, ] = ");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Определите второй размер двумерного массива : \n\t[, y] = ");
+            int y = Convert.ToInt32(Console.ReadLine());
+
+            int[,] arr = new int[x, y];
+            //Заполнить массив рандомными значениями по заданному ранее диапазону
         }   
     }
 }
