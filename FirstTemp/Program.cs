a058e3412bbb9edbeda86ac88d3e19b3e5f8c389
CEPIII <seroboy@yandex.ru>
2020-12-06 20:42:30 +0300

    Подсчет суммы двумерного массива завершен

--- FirstTemp/Program.cs
+++ FirstTemp/Program.cs
@@ -11,6 +11,15 @@ namespace FirstTemp
             /// Подсчитать сумму чисел в двухмерном массиве
             
             int[,] arr = new int[,] { { 11, 22, 31 }, { 4, 53, 6 }, { 7, 81, 90 } };
+            int lengthArr = arr.Length;
+            Console.WriteLine(lengthArr);
+
+            int summ = 0;
+            foreach (var x in arr)
+            {
+                summ += x;
+            }
+            Console.WriteLine($"Сумма двумерного массива составляет ; \n summ = {summ}");
         }
 
     
