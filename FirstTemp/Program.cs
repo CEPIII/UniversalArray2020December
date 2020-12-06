cbe09d2afcdf6a7b06c19e4224080595cebd776f
CEPIII <seroboy@yandex.ru>
2020-12-06 20:33:28 +0300

    Подсчитать сумму чисел в двухмерном массиве

--- FirstTemp/Program.cs
+++ FirstTemp/Program.cs
@@ -7,6 +7,12 @@ namespace FirstTemp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            /// Подсчитать сумму чисел в двухмерном массиве
+            
+            int[,] arr = new int[,] { { 11, 22, 31 }, { 4, 53, 6 }, { 7, 81, 90 } };
         }
+
+    
     }
 }
