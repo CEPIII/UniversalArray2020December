f091dba9787b6af90e50bc7aa0a39f9a782aa80b
CEPIII <seroboy@yandex.ru>
2020-12-06 21:08:46 +0300

    Добавил второй вариант суммы елементов двумерного массива

--- FirstTemp/Program.cs
+++ FirstTemp/Program.cs
@@ -11,6 +11,19 @@ namespace FirstTemp
             /// Подсчитать сумму чисел в двухмерном массиве
             
             int[,] arr = new int[,] { { 11, 22, 31 }, { 4, 53, 6 }, { 7, 81, 90 } };
+
+            /*
+            int sum = 0;
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    sum += arr[i, j];
+                }
+            }
+            Console.WriteLine(sum + "\n");      */
+
+            
             int lengthArr = arr.Length;
             Console.WriteLine(lengthArr);
 
@@ -20,8 +33,18 @@ namespace FirstTemp
                 summ += x;
             }
             Console.WriteLine($"Сумма двумерного массива составляет ; \n summ = {summ}");
-        }
+            
+            /*
+            Console.WriteLine("====Заполнить двумерный массив случайными числами====");
+            int[,] arrTwo10 = new int[10, 10];
+            Random rnd = new Random();
+            for (int i = 0; i < arrTwo10.Length; i++)
+            {
 
-    
+            }
+            */
+
+
+        }   
     }
 }
