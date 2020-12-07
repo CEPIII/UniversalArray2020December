f708f20ab4a25f290bf43857772257c511af1f0f
CEPIII <seroboy@yandex.ru>
2020-12-07 03:27:31 +0300

    Массив заполнил, дальше минимальный, максимальный и сумма

--- FirstTemp/Program.cs
+++ FirstTemp/Program.cs
@@ -15,6 +15,20 @@ namespace FirstTemp
 
             int[,] arr = new int[x, y];
             //Заполнить массив рандомными значениями по заданному ранее диапазону
+            Random rnd = new Random();
+            //Заодно найду максимальный элемент, минимальный и сумму всех элементов
+            int min, max;
+            Console.WriteLine($"Заполненный массив \n\t int[,] arr = new int[{x}, {y}]");
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                Console.Write("{");
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    arr[i, j] = rnd.Next(0, diapazon);
+                    Console.Write($"  {arr[i, j]}  ");
+                }
+                Console.Write("}\n");
+            }
         }   
     }
 }
